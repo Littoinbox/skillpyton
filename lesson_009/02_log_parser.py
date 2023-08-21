c93201d6378ab3e1e06b14ac06e2c574768578cb
Littoinbox <litto@inbox.ru>
2023-08-21 13:28:52 +0300

    9 урок 2 задание конец с добавлением сортировки

--- lesson_009/02_log_parser.py
+++ lesson_009/02_log_parser.py
@@ -28,29 +28,47 @@ import os.path
 
 class EventsRestruckt:
     file = ''
+    outFile = ""
+    statistic = {}
+    group = ""
 
-    def __init__(self, fileName):
-        self.file = fileName
+    def __init__(self, inputFile, outFile, group='minute'):
+        self.file = inputFile
+        self.outFile = outFile
+        self.setGroup(group)
+
+    def setGroup(self, group):
+        if group == 'month':
+            self.group = '%Y-%m'
+        elif group == 'minute':
+            self.group = '%Y-%m-%d %H:%M'
+        elif group == 'year':
+            self.group = '%Y'
+        elif group == 'hours':
+            self.group = '%Y-%m-%d %H'
+        elif group == 'days':
+            self.group = '%Y-%m-%d'
 
     def create_stat(self):
         with open(os.path.join(os.path.dirname(__file__), self.file), 'r') as file:
-            last_date = datetime.datetime.now()
             for line in file:
                 l = line.split("] ")
                 date = datetime.datetime.strptime(l[0].replace('[', ''), '%Y-%m-%d %H:%M:%S.%f')
-                if date.minute == last_date.minute:
-                    if date.hour == last_date.hour:
-                        if date.date() == last_date.date:
-                            if l[1].replace("\n") != "NOK":
-                                if date.format('%Y-%m-%d %H:%M') in self.statistic:
-                                    self.statistic[date.format('%Y-%m-%d %H:%M')] += 1
-                                else:
-                                    self.statistic[date.format('%Y-%m-%d %H:%M')] = 1
+                if l[1].replace("\n", "") == "NOK":
+                    if date.strftime(self.group) in self.statistic:
+                        self.statistic[date.strftime(self.group)] += 1
+                    else:
+                        self.statistic[date.strftime(self.group)] = 1
+    def writeStat(self):
+        if len(self.outFile) >0 and len(self.statistic) >0:
+            file = open(os.path.join(os.path.dirname(__file__), self.outFile), 'w')
+            for key in self.statistic:
+                   file.write(str(key)+" "+ str(self.statistic[key])+"\r")
 
 
-
-newEvent = EventsRestruckt('events.txt')
+newEvent = EventsRestruckt('events.txt', 'newEvent.txt')
 newEvent.create_stat()
+newEvent.writeStat()
 
 # После зачета первого этапа нужно сделать группировку событий
 #  - по часам
