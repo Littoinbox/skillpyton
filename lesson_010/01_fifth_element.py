edcc429c6eddd268e515ef176487cdfcd483cd19
Littoinbox <litto@inbox.ru>
2023-09-05 17:22:57 +0300

    10 урок 2 задание

--- lesson_010/01_fifth_element.py
+++ lesson_010/01_fifth_element.py
@@ -3,12 +3,17 @@
 # Умножить константу BRUCE_WILLIS на пятый элемент строки, введенный пользователем
 
 BRUCE_WILLIS = 42
-
-input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
-leeloo = int(input_data[4])
-result = BRUCE_WILLIS * leeloo
-print(f'- Leeloo Dallas! Multi-pass № {result}!')
-
+try:
+    input_data = input('Если хочешь что-нибудь сделать, сделай это сам: ')
+    leeloo = int(input_data[4])
+    result = BRUCE_WILLIS * leeloo
+    print(f'- Leeloo Dallas! Multi-pass № {result}!')
+except ValueError as e:
+   print (f'Невозможно преобразовать к числу {input_data}, {e}')
+except IndexError as e:
+    print(f'выход за границы списка {input_data}, {e}')
+except Exeption:
+    print(f'остальные исключения {Exeption}')
 # Ообернуть код и обработать исключительные ситуации для произвольных входных параметров
 # - ValueError - невозможно преобразовать к числу
 # - IndexError - выход за границы списка
--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import random
 
 # День сурка
 #
@@ -19,6 +20,79 @@
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
 
+class IamGodError(Exception):
+    def returnError(self):
+        return "Исключение бога"
+
+
+class DrunkError(Exception):
+    def returnError(self):
+        return "Исключение пьяни"
+
+
+class CarCrashError(Exception):
+    def returnError(self):
+        return "Исключение Автоаварии"
+
+
+class GluttonyError(Exception):
+    def returnError(self):
+        return "Исключение чревоугодья"
+
+
+class DepressionError(Exception):
+    def returnError(self):
+        return "Исключение депрессии"
+
+
+class SuicideError(Exception):
+    def returnError(self):
+        return "Исключение суицидв"
+
+
+def randomExeption():
+    errorNumber = random.randint(1, 6)
+    if errorNumber == 1:
+        return IamGodError()
+    if errorNumber == 2:
+        return DrunkError()
+    if errorNumber == 3:
+        return CarCrashError()
+    if errorNumber == 4:
+        return GluttonyError()
+    if errorNumber == 5:
+        return DepressionError()
+    if errorNumber == 6:
+        return SuicideError()
+
+
+def one_day():
+    i = random.randint(1, 13)
+    if i == 13:
+        raise randomExeption()
+    else:
+        return random.randint(1, 7)
+
+
+karmaSumma = 0
+day = 0
+while karmaSumma <= ENLIGHTENMENT_CARMA_LEVEL:
+    try:
+        day += 1
+        karmaSumma += one_day()
+    except  IamGodError as err:
+        print(f"Произошла ошибка {err.__class__.__name__}, Текст ощибки {err.returnError()}")
+    except  DrunkError as err:
+        print(f"Произошла ошибка {err.__class__.__name__}, Текст ощибки {err.returnError()}")
+    except  CarCrashError as err:
+        print(f"Произошла ошибка {err.__class__.__name__}, Текст ощибки {err.returnError()}")
+    except  GluttonyError as err:
+        print(f"Произошла ошибка {err.__class__.__name__}, Текст ощибки {err.returnError()}")
+    except  DepressionError as err:
+        print(f"Произошла ошибка {err.__class__.__name__}, Текст ощибки {err.returnError()}")
+    except  SuicideError as err:
+        print(f"Произошла ошибка {err.__class__.__name__}, Текст ошибки {err.returnError()}")
+    finally:
+        print(f"Прошел {day} день, карма равна {karmaSumma}")
 # https://goo.gl/JnsDqu
