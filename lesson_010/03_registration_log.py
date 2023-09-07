32dd1c5d11c61e801106d48054b56c1a257c6f37
Littoinbox <litto@inbox.ru>
2023-09-07 18:23:47 +0300

    10 урок 3 задание

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -1,4 +1,7 @@
 # -*- coding: utf-8 -*-
+import os
+import re
+
 
 # Есть файл с протоколом регистраций пользователей на сайте - registrations.txt
 # Каждая строка содержит: ИМЯ ЕМЕЙЛ ВОЗРАСТ, разделенные пробелами
@@ -21,5 +24,54 @@
 # - поле емейл НЕ содержит @ и .(точку): NotEmailError (кастомное исключение)
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
+class NotNameError(Exception):
+    pass
 
-# TODO здесь ваш код
+
+class NotEmailError(Exception):
+    pass
+
+
+def checkName(name):
+    pattern = r'[\d]'
+    match = re.search(pattern, name)
+    return not bool(match)
+
+
+def checkEmail(email):
+    pattern = r'[@]'
+    pattern2 = r'[\.]'
+    dog = re.search(pattern, email)
+    coma = re.search(pattern2, email)
+    return bool(dog) and bool(coma)
+
+
+def writeToFile(file, string):
+    try:
+        file.write(string)
+    except IOError as error:
+        print(f'Не могу записать в файл{file} строку {string}')
+
+
+goodfile = open('registrations_good.log', 'w')
+badfile = open('registrations_bad.log', 'w')
+filename = "registrations.txt"
+with open(os.path.join(os.path.dirname(__file__), filename), 'r', encoding='utf8') as file:
+    for line in file:
+        try:
+            map = line.split(' ')
+
+            if len(map) != 3:
+                raise ValueError("НЕ присутсвуют все три поля")
+            if not checkName(map[0]):
+                raise NotNameError("поле имени содержит НЕ только буквы")
+            if not checkEmail(map[1]):
+                raise NotEmailError("поле емейл НЕ содержит @ и .(точку)")
+            if int(map[2]) < 10 or int(map[2]) > 99 or type(int(map[2])) != int:
+                raise ValueError("поле возраст НЕ является числом от 10 до 99")
+            writeToFile(goodfile, line )
+        except (ValueError, NotNameError, NotEmailError) as e:
+            writeToFile(badfile, line+" "+ e.__class__.__name__+"\n")
+
+goodfile.close()
+badfile.close()
