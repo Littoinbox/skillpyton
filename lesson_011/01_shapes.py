1e2abbccdde62f3e90402aa5dd530a17b9b99686
Littoinbox <litto@inbox.ru>
2023-12-20 14:32:24 +0300

    11 урок 1 задание

--- lesson_011/01_shapes.py
+++ lesson_011/01_shapes.py
@@ -14,12 +14,27 @@ import simple_draw as sd
 
 
 def get_polygon(n):
-    pass
-    # TODO здесь ваш код
+    def draw_figure(point, angle=0, long=200, peaks=n):
+        step = round(360 / peaks, 0)
+        last_point = point
+        last_peaks = peaks - 1
+        for i in range(0, peaks):
+            new_angle = step * i + angle
+            if i == last_peaks:
+                sd.line(last_point, point, width=3)
+            else:
+                vector = sd.get_vector(start_point=last_point, angle=new_angle, length=long, width=3)
+                vector.draw()
+                last_point = vector.end_point
+
+    return draw_figure
 
 
-draw_triangle = get_polygon(n=3)
-draw_triangle(point=sd.get_point(200, 200), angle=13, length=100)
+
+
+draw_triangle = get_polygon(n=4)
+
+draw_triangle(point=sd.get_point(200, 200), angle=13, long=100)
 
 
 sd.pause()
